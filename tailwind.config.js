c11d84782149438da58c2ef8ccf207d148633ca9
Sachin Borse <dev.sachinborse@gmail.com>
2023-01-02 13:18:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        gotham:['Gotham Pro','sans-serif']
+      },
+      colors:{
+        white:'#FFFFFF',
+        orange:{
+          100: '#FEEDD3',
+          200: '#FE7865',
+          300: '#E6604D',
+          400: '#F55C46'
+        },
+        blue:{
+          100: '#F5F8FC',
+          200: '#EFEDFE',
+          300: '#5BD5E6'
+        },
+        green: {
+          100: '#FFFBF8',
+          200: '#E8FBF7',
+          300: '#5BE6B0'
+        },
+        gray: {
+          100: '#ECECEE',
+          200: '#DFDAFE',
+          300: '#cccfce'
+        },
+        pink:{
+          100: '#FEDAE8'
+        },
+        black: {
+          100: '#2C@CE3F',
+          200: '#141517',
+          300: '#2C2E3F',
+          400: '#1D2539'
+        },
+        yellow:{
+          100: '#FEC365'
+        },
+      },
+      spacing:{
+        144: '36rem'
+      },
+    },
+  },
+  plugins: [],
+}
